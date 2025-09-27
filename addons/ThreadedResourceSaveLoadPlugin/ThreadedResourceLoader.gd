8d6797d6d3e5f81a1e8d8bbbb76346130044ef41
Sandra Bullock <sandra@bullock>
2025-09-27 17:43:15 +0300

    loader updated to v.2.0.0

--- addons/ThreadedResourceSaveLoadPlugin/ThreadedResourceLoader.gd
+++ addons/ThreadedResourceSaveLoadPlugin/ThreadedResourceLoader.gd
@@ -1,3 +1,6 @@
+## doesn't filter duplicates by resiurce path param, godot itself candles the 
+##	cache check for already loaded reses
+
 extends Node
 class_name ThreadedResourceLoader
 
@@ -7,14 +10,17 @@ signal loadProgress(completedCount: int, totalResources: int, resource: Resource
 # typing: loadedFiles -> Dictionary[String, Resource]
 signal loadCompleted(loadedFiles: Dictionary)
 signal loadError(path: String)
-signal loadReady()
+signal becameIdle()
 
 static var ignoreWarnings: bool = false
 
 var _semaphore: Semaphore
 var _mutex: Mutex
-var _loadThreads: Array[Thread] = []
-var _loadQueue: Array[Array] = []
+var _threads: Array[Thread] = []
+# curently processing queue
+var _activeQueue: Array[Array] = []
+# queue awaiting for `start` call
+var _idleQueue: Array[Array] = []
 var _totalResourcesAmount: int = 0
 var _completedResourcesAmount: int = 0
 var _failedResourcesAmount: int = 0
@@ -27,6 +33,9 @@ var _currentThreadsAmount: int = 0
 #	to prevent confusion for reses with the same names)
 # typing: Dictionary[String, String]
 var _resesPathToNameMap: Dictionary = {}
+# flag for start calls during cleaning (stopping) stage
+var _auto_start_on_ready: bool = false
+var _auto_start_on_ready_thread_amount: int = 0
 
 
 func _init() -> void:
@@ -35,69 +44,94 @@ func _init() -> void:
 
 
 func is_idle() -> bool:
-	return not _loadingHasStarted
+	_mutex.lock()
+	var result = not _loadingHasStarted
+	_mutex.unlock()
+	return result
 
 
 func get_current_threads_amount() -> int:
-	return _currentThreadsAmount
+	_mutex.lock()
+	var result = _currentThreadsAmount
+	_mutex.unlock()
+	return result
 
 
 func add(resources: Array[Array]) -> ThreadedResourceLoader:
 	_mutex.lock()
-	if _loadingHasStarted:
-		_mutex.unlock()
-		push_error("loading has already started, current call ignored")
-		return self
 	
 	for params in resources:
+		# not enough params
 		if params.size() < 2: 
 			push_error("too few arguments in params array, will be ignored")
 			continue
+		# key param has incorrect type 
 		elif typeof(params[0]) != TYPE_STRING and typeof(params[0]) != TYPE_STRING_NAME:
 			push_error("invalid param value: \"{0}\" for resource key, it should be a type of String or StringName, will be ignored".format([params[0]]))
 			continue
+		# path param has incorrect type 
 		elif (typeof(params[1]) != TYPE_STRING and typeof(params[1]) != TYPE_STRING_NAME) or params[1].strip_edges() == "":
 			push_error("invalid param value: \"{0}\" for resource path, it should be a non-empty String or StringName, will be ignored".format([params[1]]))
 			continue
+		# skip if key already exists
+		elif params[0].strip_edges() != "" and _keyExist(params[0]):
+			if not ThreadedResourceLoader.ignoreWarnings:
+				push_warning("key \"{0}\" already exists, resource will be ignored".format(params[0]))
 		
-		_loadQueue.append(params)
+		_idleQueue.append(params)
 	
 	_mutex.unlock()
 	
 	return self
 
 
+func _keyExist(key: String) -> bool:
+	return _resesPathToNameMap.has(key) or _idleQueue.any(func(params: Array) -> bool: return params[0] == key)
+
+
 func start(threadsAmount: int = OS.get_processor_count() - 1) -> ThreadedResourceLoader:
 	_mutex.lock()
-	if _loadingHasStarted:
+	if _isStopping:
+		push_warning("currently in the cleaning stage, the start will be delayed")
+		_auto_start_on_ready = true
+		_auto_start_on_ready_thread_amount = threadsAmount
 		_mutex.unlock()
-		push_error("loading has already started, current call ignored")
 		return self
 	
-	_totalResourcesAmount = _loadQueue.size()
+	mergeIdleQueue()
+	_totalResourcesAmount += _idleQueue.size()
 	
 	if _totalResourcesAmount == 0:
 		if not ThreadedResourceLoader.ignoreWarnings:
 			push_warning("load queue is empty, immediate finish loading signal emission")
 		call_deferred("emit_signal", "loadCompleted", _loadedFiles)
 		_mutex.unlock()
-		_clearDataAfterLoad.call_deferred()
+		if _loadingHasStarted:
+			_stopLoadThreads.call_deferred()
+		else:
+			_clearDataAfterLoad.call_deferred()
 		return self
 	
-	_loadingHasStarted = true
+	if not _loadingHasStarted:
+		_loadingHasStarted = true
 	
-	# Create thread pool for this loading session
-	_initThreadPool(threadsAmount)
-	_initResesPathToNameMap()
+		# Create thread pool for this loading session
+		_initThreadPool(threadsAmount)
 	
-	call_deferred("emit_signal", "loadStarted", _totalResourcesAmount)
+		call_deferred("emit_signal", "loadStarted", _totalResourcesAmount)
 	
-	for _i in range(_currentThreadsAmount):
-		_semaphore.post.call_deferred()
+		for _i in range(_currentThreadsAmount):
+			_semaphore.post.call_deferred()
 	
+	_idleQueue.clear()
 	_mutex.unlock()
 	
 	return self
+
+
+func mergeIdleQueue() -> void:
+	_activeQueue.append_array(_idleQueue)
+	_processPathToNameMap()
 
 
 func _initThreadPool(threadsAmount: int) -> void:
@@ -105,14 +139,16 @@ func _initThreadPool(threadsAmount: int) -> void:
 	var thread: Thread
 	for i in range(actualThreadsNeeded):
 		thread = Thread.new()
-		_loadThreads.append(thread)
+		_threads.append(thread)
 		thread.start(_loadThreadWorker)
 	_currentThreadsAmount = actualThreadsNeeded
 
 
-func _initResesPathToNameMap() -> void:
+# when _idleQueue been merged with _activeQueue after loading has started
+#	duplicates been filtered in mergeIdleQueue
+func _processPathToNameMap() -> void:
 	var resource_name: String
-	for loadItem in _loadQueue:
+	for loadItem in _idleQueue:
 		resource_name = loadItem.pop_front()
 		# if pased name is empty - use resource path
 		if resource_name.is_empty():
@@ -130,11 +166,12 @@ func _loadThreadWorker() -> void:
 			_mutex.unlock()
 			break
 		
-		if _loadQueue.is_empty():
+		if _activeQueue.is_empty():
 			_mutex.unlock()
 			continue
 		
-		var loadItem: Array = _loadQueue.pop_back()
+		var loadItem: Array = _activeQueue.pop_back()
+		
 		_mutex.unlock()
 		
 		var resource: Resource = ResourceLoader.load.callv(loadItem)
@@ -165,7 +202,7 @@ func _loadThreadWorker() -> void:
 		else:
 			_mutex.unlock()
 			
-			if not _loadQueue.is_empty():
+			if not _activeQueue.is_empty():
 				_semaphore.post()
 
 
@@ -181,7 +218,7 @@ func _stopLoadThreads() -> void:
 	for _i in range(_currentThreadsAmount):
 		_semaphore.post()
 	
-	for thread in _loadThreads:
+	for thread in _threads:
 		# not checking for alive coz thread coud exit naturaly on finished the work
 		# so closing all the threads been opened anyway
 		if thread.is_started():
@@ -195,8 +232,8 @@ func _clearDataAfterLoad() -> void:
 	_mutex.lock()
 	
 	# Clear all data for next use
-	_loadQueue.clear()
-	_loadThreads.clear()
+	_activeQueue.clear()
+	_threads.clear()
 	_loadedFiles = {}
 	_totalResourcesAmount = 0
 	_completedResourcesAmount = 0
@@ -204,10 +241,17 @@ func _clearDataAfterLoad() -> void:
 	_isStopping = false
 	_loadingHasStarted = false
 	_currentThreadsAmount = 0
+	_resesPathToNameMap.clear()
 	
+	if _idleQueue.is_empty():
+		_auto_start_on_ready = false
+		_auto_start_on_ready_thread_amount = 0
+	elif _auto_start_on_ready:
+		call_deferred("start", _auto_start_on_ready_thread_amount)
+		
 	_mutex.unlock()
 	
-	loadReady.emit()
+	becameIdle.emit()
 
 
 # force threads cleanup on instance freed
@@ -217,15 +261,15 @@ func _notification(what: int) -> void:
 		# Force immediate thread cleanup when being deleted
 		_mutex.lock()
 		_isStopping = true
-		_mutex.unlock()
-		
+			
 		# don't use separate func coz ref will be invalid
 		for _i in range(_currentThreadsAmount):
 			_semaphore.post()
 		
-		for thread in _loadThreads:
+		for thread in _threads:
 			if thread.is_started():
 				thread.wait_to_finish()
+		_mutex.unlock()
 
 
 # cleanup for singleton remove / plugin disabled etc.
